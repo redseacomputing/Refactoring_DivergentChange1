8800f87a839f2a30233cd25f92014ef778a4b1ce
christopher <christopher.zelezny@mailbox.org>
2021-04-02 22:53:46 +0200

    Refactor divergent change

---
+++ src/main/java/divergentchange/Account.java
@@ -0,0 +1,32 @@
+package divergentchange;
+
+public class Account {
+
+    private final int accountNumber;
+    private double balance = 0;
+
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void credit(double amount) {
+        balance += amount;
+    }
+
+    public void debit(double amount) {
+        balance -= amount;
+    }
+
+    public String toXml() {
+        return "<account><id>" + getAccountNumber() + "</id>" +
+                "<balance>" + getBalance() + "</balance></account>";
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
